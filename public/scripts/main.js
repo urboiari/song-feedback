a44b82ac4161b2a35ce628f0507c2f0c61fc0dfa
Ari Rivera <soewarsoari@gmail.com>
2020-08-25 23:13:57 -0700

    Comment dissappears on post

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -14,4 +14,21 @@ function postComment() {
   };
 
   fetch('/comments', commentObj);
+
+  dissappearElement('#comment-text');
+  dissappearElement('#comment-btn');
+}
+
+async function dissappearElement(elementId) {
+    let element = document.querySelector(elementId);
+    await dissappear(elementId, 3);
+    element.parentNode.removeChild(element);
+}
+
+function dissappear(elementId, seconds) {
+  return new Promise((resolve, reject) => {
+    document.querySelector(elementId).style.transform = `opacity ${seconds}s`;
+    document.querySelector(elementId).style.opacity = '0';
+    setTimeout(() => resolve('complete'), seconds * 1000);
+  })
 }
