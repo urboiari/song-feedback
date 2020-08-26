b4832bf715e4bed70f526f9038b7e958530876cd
Ari Rivera <soewarsoari@gmail.com>
2020-08-26 13:08:14 -0700

    Transition is added by dissappear function

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,10 @@
+// ADD FUNCTIONALITY TO COMMENT BUTTON
 let commentBtn = document.querySelector('#comment-btn');
-
 commentBtn.addEventListener('click', postComment);
 
+// COMMENT BUTTON FUNCTION
 async function postComment() {
+  // TAKE COMMENT AND TURN IT INTO A POST OBJECT
   let comment = document.querySelector('#comment-text').value;
   let data = {comment}
   let commentObj = {
@@ -13,12 +15,14 @@ async function postComment() {
     body: JSON.stringify(data),
   };
 
+  // SEND COMMENT TO THE DB AND DISSAPPEAR THE COMMENT ELEMENTS
   let response = await fetch('/comments', commentObj)
   const json = await response.json();
   console.log(json);
   dissappearElement('#comment-text');
   dissappearElement('#comment-btn');
 
+  // RECIEVE ALL THE COMMENTS FROM THE DATABASE
   let allComments = await fetch('/comments');
   let jsonComments = await allComments.json();
   console.log(jsonComments);
@@ -32,6 +36,7 @@ async function dissappearElement(elementId) {
 
 function dissappear(elementId, seconds) {
   return new Promise((resolve, reject) => {
+    document.querySelector(elementId).style.transition = `opacity ${seconds}s`;
     document.querySelector(elementId).style.opacity = '0';
     setTimeout(() => resolve('complete'), seconds * 1000);
   })
