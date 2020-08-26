d0c9ea41922ded0285102150608a110cc459d083
Ari Rivera <soewarsoari@gmail.com>
2020-08-26 12:42:09 -0700

    Insert comments to db with timestamp

--- index.js
+++ index.js
@@ -10,9 +10,33 @@ let database = new Datastore('commentBank.db');
 database.loadDatabase();
 
 app.post('/comments', (request, response) => {
+
+
   console.log(request.body);
+  let timestamp = makeTimestamp();
+  database.insert({
+    comment: request.body,
+    timestamp: timestamp
+  });
   response.json({
     status: 'success',
-    comment: request.body.comment
+    comment: request.body.comment,
+    timestamp: timestamp
   });
 });
+
+function makeTimestamp() {
+  let currentTime = new Date;
+  let currentMonth = currentTime.getMonth();
+  let currentDate = currentTime.getDate();
+  let currentHours = currentTime.getHours();
+  let currentMinutes = currentTime.getMinutes();
+
+  currentMinutes = (currentMinutes < 10 ? '0' : '') + currentMinutes;
+
+  let timeOfDay = (currentHours < 12) ? 'am' : 'pm';
+  currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+  currentHours = (currentHours == 0) ? 12 : currentHours;
+
+  return `${currentMonth}/${currentDate} ${currentHours}:${currentMinutes} ${timeOfDay}`;
+}
