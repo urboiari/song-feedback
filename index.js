5153ee997074411cfe8009ff08831296d19460e8
Ari Rivera <soewarsoari@gmail.com>
2020-08-26 12:24:48 -0700

    Add nedb and created a database for comments

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const app = express();
+const Datastore = require('nedb');
 
+const app = express();
 app.listen(8000, () => console.log('listening at 8000'));
 app.use(express.static('public'));
 app.use(express.json({limit: '1kb'}));
+
+let database = new Datastore('commentBank.db');
+database.loadDatabase();
 
 app.post('/comments', (request, response) => {
   console.log(request.body);
